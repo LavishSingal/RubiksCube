23ae0c248656ac726b6b58c5a2c0238fb83c7994
Lavish Singal <lavish@Lavishs-MacBook-Air.local>
2025-06-07 22:18:00 +0530

    random

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="bg-white text-black h-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl">Helllo</h1>
-      <Canvas className="w-full h-full p-4 bg-gray-50" camera={{ position: [5, 5, 5] }} raycaster={{ firstHitOnly: true }}>
+      <Canvas className="w-full h-full m-4 bg-gray-500" camera={{ position: [5, 5, 5] }} raycaster={{ firstHitOnly: true }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
         <Cube controlsRef={controlsRef} />
