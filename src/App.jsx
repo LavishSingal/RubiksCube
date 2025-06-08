a6574f4e26e55108517654eb589d34d25d0e0449
Lavish Singal <lavish@Lavishs-MacBook-Air.local>
2025-06-08 21:16:14 +0530

    bugs resolved

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
 
   return (
     <div className="bg-white text-black h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl">Helllo</h1>
       <Canvas className="w-full h-full m-4 bg-gray-500" camera={{ position: [5, 5, 5] }} raycaster={{ firstHitOnly: true }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
